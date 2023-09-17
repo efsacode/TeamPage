5847a40e8bdc3bc55455ef9384a453b143948a26
efsa <113553752+efsacode@users.noreply.github.com>
2023-09-17 03:17:55 +0300

    design
    (background image revize edilebilir)

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 jQuery(document).ready(function($) {
-	var mastheadheight = $('.ds-header').outerHeight();
+	var mastheadheight = $('.header').outerHeight();
 	//console.log(mastheadheight);
 	$(".ds-banner,.ds-main-section").css("margin-top" , mastheadheight);
 
 	$(window).scroll(function(){
 	    if ($(window).scrollTop() >= 10) {
-	        $('.ds-header').addClass('ds-fixed-header');
+	        $('.header').addClass('ds-fixed-header');
 	    }
 	    else {
-	        $('.ds-header').removeClass('ds-fixed-header');
+	        $('.header').removeClass('ds-fixed-header');
 	    }
 	}).scroll();
 });
